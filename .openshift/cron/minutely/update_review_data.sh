4695e639e2d21351ce899760bb58ef78db79ae66
Eric D. Helms <ericdhelms@gmail.com>
2015-12-17 12:05:28 -0500

    Point at application hostname for cron job

--- .openshift/cron/minutely/update_review_data.sh
+++ .openshift/cron/minutely/update_review_data.sh
@@ -3,7 +3,7 @@
 # Sadly you can't do "*/15 * * * *" in Openshift, so this is a hack
 # Runs every minute but only does work on */15
 
-cmd="curl -X GET http://localhost/api/reviews/update/"
+cmd="curl -X GET http://$OPENSHIFT_APP_DNS/api/reviews/update/"
 case `date +%M` in
 00)
   `$cmd`
